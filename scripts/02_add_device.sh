5fd885ce925e1695b9d3c3bb9238831768fc0e08
1737255535 <1737255535@qq.com>
2026-06-05 11:41:22 +0800

    fix: add explicit DEVICE_DTS to fix DTS filename mismatch

--- scripts/02_add_device.sh
+++ scripts/02_add_device.sh
@@ -57,6 +57,7 @@ define Device/panther_x2
 $(call Device/Legacy/rk3566,$(1))
   DEVICE_VENDOR := Panther
   DEVICE_MODEL := X2
+  DEVICE_DTS := rk3568/rk3566-panther-x2
   SUPPORTED_DEVICES += panther,x2
   DEVICE_PACKAGES += kmod-brcmfmac cypress-firmware-43455-sdio wpad-basic-mbedtls
 endef
